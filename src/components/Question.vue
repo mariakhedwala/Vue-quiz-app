6b1826b24415ec6e98172a674a68a07bfda57a43
Maria Khedwala <mariakhedwala@gmail.com>
2023-06-19 21:09:02 +0530

    complete quiz app

---
+++ src/components/Question.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const emit = defineEmits(["selectedOption"]);
+
+const { question } = defineProps(["question"]);
+
+const selectedOption = (isCorrect) => {
+  emit("selectedOption", isCorrect);
+};
+</script>
+
+<template>
+  <div class="question-container">
+    <h1 class="question">{{ question.text }}</h1>
+  </div>
+  <ul class="options-container">
+    <li
+      v-for="option in question.options"
+      :key="option.id"
+      class="option"
+      @click="selectedOption(option.isCorrect)"
+    >
+      <p class="option-label">{{ option.label }}</p>
+      <p class="option-value">{{ option.text }}</p>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.question-container {
+  margin-top: 20px;
+}
+.question {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+.option {
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+.option-label {
+  background-color: bisque;
+  width: 60px;
+  height: 50px;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.option-value {
+  background-color: rgb(244, 239, 239);
+  width: 100%;
+  font-size: 30px;
+  padding: 0 20px;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+
+const { correctAnsCount, totalQuestions } = defineProps([
+  "correctAnsCount",
+  "totalQuestions",
+]);
+</script>
+
+<template>
+  <div class="results">
+    <p>your results...</p>
+    <h1>{{ correctAnsCount }}/{{ totalQuestions }}</h1>
+    <RouterLink to="/">Go back</RouterLink>
+  </div>
+</template>
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+.results p {
+  font-size: 48px;
+}
+.results h1 {
+  font-size: 80px;
+}
+</style>
